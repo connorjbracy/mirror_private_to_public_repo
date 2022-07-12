86dafcf006795b6be9e4125f75508b137749958b
Connor Bracy <cjb2250@columbia.edu>
2022-07-12 09:43:48 -0400

    Added debug statement header and redirecting stderr to stdout for deterministic debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,74 +1,77 @@
 #!/bin/sh
 
+export PS4="################################################################################ "
+
 set -e
 set -x
+{
+  if [ -z "$INPUT_SOURCE_FILE" ]
+  then
+    echo "Source file must be defined"
+    return 1
+  fi
 
-if [ -z "$INPUT_SOURCE_FILE" ]
-then
-  echo "Source file must be defined"
-  return 1
-fi
+  if [ -z "$INPUT_GIT_SERVER" ]
+  then
+    INPUT_GIT_SERVER="github.com"
+  fi
 
-if [ -z "$INPUT_GIT_SERVER" ]
-then
-  INPUT_GIT_SERVER="github.com"
-fi
+  if [ -z "$INPUT_DESTINATION_BRANCH" ]
+  then
+    INPUT_DESTINATION_BRANCH=main
+  fi
+  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 
-if [ -z "$INPUT_DESTINATION_BRANCH" ]
-then
-  INPUT_DESTINATION_BRANCH=main
-fi
-OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
+  CLONE_DIR=$(mktemp -d)
 
-CLONE_DIR=$(mktemp -d)
+  echo "Cloning destination git repository"
+  git config --global user.email "$INPUT_USER_EMAIL"
+  git config --global user.name "$INPUT_USER_NAME"
+  git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$INPUT_REPO_LEVEL_SEC@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "Cloning destination git repository"
-git config --global user.email "$INPUT_USER_EMAIL"
-git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$INPUT_REPO_LEVEL_SEC@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+  if [ ! -z "$INPUT_RENAME" ]
+  then
+    echo "Setting new filename: ${INPUT_RENAME}"
+    DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
+  else
+    DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+  fi
 
-if [ ! -z "$INPUT_RENAME" ]
-then
-  echo "Setting new filename: ${INPUT_RENAME}"
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
-else
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-fi
+  echo "Showing cwd contents"
+  ls -la .
 
-echo "Showing cwd contents"
-ls -la .
+  echo "Copying contents to git repo"
+  mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
+  if [ -z "$INPUT_USE_RSYNC" ]
+  then
+    cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
+  else
+    echo "rsync mode detected"
+    rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
+  fi
 
-echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-if [ -z "$INPUT_USE_RSYNC" ]
-then
-  cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
-else
-  echo "rsync mode detected"
-  rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
-fi
+  cd "$CLONE_DIR"
 
-cd "$CLONE_DIR"
+  if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+  then
+    echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
+    git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
+    OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
+  fi
 
-if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
-then
-  echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
-  git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
-  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
-fi
+  if [ -z "$INPUT_COMMIT_MESSAGE" ]
+  then
+    INPUT_COMMIT_MESSAGE="Update from https://$INPUT_GIT_SERVER/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
+  fi
 
-if [ -z "$INPUT_COMMIT_MESSAGE" ]
-then
-  INPUT_COMMIT_MESSAGE="Update from https://$INPUT_GIT_SERVER/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
-fi
-
-echo "Adding git commit"
-git add .
-if git status | grep -q "Changes to be committed"
-then
-  git commit --message "$INPUT_COMMIT_MESSAGE"
-  echo "Pushing git commit"
-  git push -u origin HEAD:"$OUTPUT_BRANCH"
-else
-  echo "No changes detected"
-fi
+  echo "Adding git commit"
+  git add .
+  if git status | grep -q "Changes to be committed"
+  then
+    git commit --message "$INPUT_COMMIT_MESSAGE"
+    echo "Pushing git commit"
+    git push -u origin HEAD:"$OUTPUT_BRANCH"
+  else
+    echo "No changes detected"
+  fi
+} 2>&1
