81312896945b18afa80729764a337c3af72285e6
Connor Bracy <cjb2250@columbia.edu>
2022-07-13 17:38:33 -0400

    Checking clone full repo, check out base ref, start new branch named as head ref

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,14 +53,12 @@ fi
 # fi
 
 sectionheader "Check for INPUT_GIT_SERVER = $INPUT_GIT_SERVER"
-if [ -z "$INPUT_GIT_SERVER" ]
-then
+if [ -z "$INPUT_GIT_SERVER" ]; then
   INPUT_GIT_SERVER="github.com"
 fi
 
 sectionheader "Check for INPUT_DESTINATION_BRANCH = $INPUT_DESTINATION_BRANCH"
-if [ -z "$INPUT_DESTINATION_BRANCH" ]
-then
+if [ -z "$INPUT_DESTINATION_BRANCH" ]; then
   INPUT_DESTINATION_BRANCH=main
 fi
 OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
@@ -71,11 +69,17 @@ CLONE_DIR=$(mktemp -d)
 sectionheader "Cloning public repo to tempdir = $CLONE_DIR"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$INPUT_MY_GITHUB_SECRET_PAT@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+# git clone --single-branch --branch $GITHUB_BASE_REF "https://x-access-token:$INPUT_MY_GITHUB_SECRET_PAT@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone "https://x-access-token:$INPUT_MY_GITHUB_SECRET_PAT@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+cd "$CLONE_DIR"
+
+printcmd git branch -a
+
+exit 0
 
 sectionheader "Check for INPUT_RENAME = $INPUT_RENAME"
-if [ ! -z "$INPUT_RENAME" ]
-then
+if [ ! -z "$INPUT_RENAME" ]; then
   echo "Setting new filename: ${INPUT_RENAME}"
   DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
 else
@@ -143,8 +147,7 @@ sectionheader "Copying date to file to force commit"
 printcmd date > "$CLONE_DIR/force_commit.txt"
 
 sectionheader "Check for INPUT_DESTINATION_BRANCH_CREATE = $INPUT_DESTINATION_BRANCH_CREATE"
-if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
-then
+if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]; then
   echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
   git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
   OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
@@ -152,122 +155,17 @@ fi
 sectionheader "Concluded OUTPUT_BRANCH = $OUTPUT_BRANCH"
 
 sectionheader "Check for INPUT_COMMIT_MESSAGE = $INPUT_COMMIT_MESSAGE"
-if [ -z "$INPUT_COMMIT_MESSAGE" ]
-then
+if [ -z "$INPUT_COMMIT_MESSAGE" ]; then
   INPUT_COMMIT_MESSAGE="Update from https://$INPUT_GIT_SERVER/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
 fi
 
 # echo "Adding git commit"
 sectionheader "Adding git commit"
 printcmd git add .
-if git status | grep -q "Changes to be committed"
-then
+if git status | grep -q "Changes to be committed"; then
   printcmd git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
   printcmd git push -u origin HEAD:"$OUTPUT_BRANCH"
 else
   echo "No changes detected"
 fi
-
-# # set -x
-# { # Execute in a block that feeds stderr to stdout to avoid interleaving
-
-#   statementheader "Check for GitHub Secrets PAT"
-#   exit 0
-#   # Remind users that a PAT will be needed for pushing the commit.
-#   if [ "$INPUT_MY_GITHUB_SECRET_PAT" ]; then
-#     echo "GITHUB_SECRET_PAT = $INPUT_MY_GITHUB_SECRET_PAT"
-#   else
-#     longecho "Required argument 'github_secret_pat' missing!
-#               Please review your GitHub Actions script that called this Action."
-#     exit 1
-#   fi
-
-#   # Construct path to private repo
-#   PRIVATE_REPO_DIR="$(realpath "$GITHUB_WORKSPACE/$INPUT_MY_PRIVATE_SUBDIR")"
-#   if [ ! -d "$PRIVATE_REPO_DIR" ]; then
-#     echo "Could not find the directory containing the private repo: $PRIVATE_REPO_DIR"
-#     exit 2
-#   fi
-#   PRIVATE_REPO_GIT_CONFIG_FULLNAME="$(                        \
-#     git -C "$PRIVATE_REPO_DIR" config --get remote.origin.url \
-#     | sed -nr 's|^git@github\.com:(\w+/\w+)\.git$|\1|p'       \
-#   )"
-#   if [ "$PRIVATE_REPO_GIT_CONFIG_FULLNAME" != $GITHUB_REPOSITORY ]; then
-#     echo "Using the given argument
-#       'private_subdir': $INPUT_MY_PRIVATE_SUBDIR
-
-#       'private_subdir': $INPUT_MY_PRIVATE_SUBDIR"
-#   fi
-
-#   if [ -z "$INPUT_SOURCE_FILE" ]
-#   then
-#     echo "Source file must be defined"
-#     return 1
-#   fi
-
-#   if [ -z "$INPUT_GIT_SERVER" ]
-#   then
-#     INPUT_GIT_SERVER="github.com"
-#   fi
-
-#   if [ -z "$INPUT_DESTINATION_BRANCH" ]
-#   then
-#     INPUT_DESTINATION_BRANCH=main
-#   fi
-#   OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
-
-#   CLONE_DIR=$(mktemp -d)
-
-#   echo "Cloning destination git repository"
-#   git config --global user.email "$INPUT_USER_EMAIL"
-#   git config --global user.name "$INPUT_USER_NAME"
-#   git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$INPUT_MY_GITHUB_SECRET_PAT@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
-
-#   if [ ! -z "$INPUT_RENAME" ]
-#   then
-#     echo "Setting new filename: ${INPUT_RENAME}"
-#     DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
-#   else
-#     DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-#   fi
-
-#   echo "Showing cwd contents"
-#   ls -la .
-
-#   echo "Copying contents to git repo"
-#   mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-#   if [ -z "$INPUT_USE_RSYNC" ]
-#   then
-#     cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
-#   else
-#     echo "rsync mode detected"
-#     rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
-#   fi
-
-#   cd "$CLONE_DIR"
-#   date > "$CLONE_DIR/force_commit.txt"
-
-#   if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
-#   then
-#     echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
-#     git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
-#     OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
-#   fi
-
-#   if [ -z "$INPUT_COMMIT_MESSAGE" ]
-#   then
-#     INPUT_COMMIT_MESSAGE="Update from https://$INPUT_GIT_SERVER/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
-#   fi
-
-#   echo "Adding git commit"
-#   git add .
-#   if git status | grep -q "Changes to be committed"
-#   then
-#     git commit --message "$INPUT_COMMIT_MESSAGE"
-#     echo "Pushing git commit"
-#     git push -u origin HEAD:"$OUTPUT_BRANCH"
-#   else
-#     echo "No changes detected"
-#   fi
-# } 2>&1
